b86660ed1e1ec7c4f829e3473a17598b7fe4f54e
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-10-21 13:46:51 +0900

    Image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [300, 400]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
